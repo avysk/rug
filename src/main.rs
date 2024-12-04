e202c9f8fc35e5748fd35e3c8fc9133b3b0a6cde
Alexey Vyskubov <alexey@pentode.fi>
2024-12-04 11:41:51 +0200

    Create database connection.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
+use futures::executor::block_on;
 use platform_dirs::UserDirs;
+use sea_orm::{Database, DbErr};
 use std::path::PathBuf;
 
 fn main() {
-    let db = create_db_path();
-    println!("{:?}", db);
+    if let Err(err) = block_on(run()) {
+        panic!("Error connecting to database: {}", err);
+    }
 }
 
 fn create_db_path() -> PathBuf {
     let mut db = UserDirs::new().unwrap().document_dir;
     db.push("rug.sqlite");
     db
+}
+
+fn create_db_url() -> String {
+    let mut uri = PathBuf::from("sqlite:");
+    let db = create_db_path();
+    uri.push(db);
+    uri.to_str().unwrap().to_string()
+}
+
+async fn run() -> Result<(), DbErr> {
+    let db_url = create_db_url();
+    let conn = Database::connect(db_url);
+    Ok(())
 }
 
 #[cfg(test)]
