91725563197440833e2b0f5bef71f34a910ac272
Alexey Vyskubov <alexey@pentode.fi>
2024-12-04 10:54:03 +0200

    Create a path to database.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use platform_dirs::UserDirs;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let db = create_db_path();
+    println!("{:?}", db);
+}
+
+fn create_db_path() -> PathBuf {
+    let mut db = UserDirs::new().unwrap().document_dir;
+    db.push("rug.sqlite");
+    db
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_path_ends_with_database_name() {
+        let db = create_db_path();
+        assert!(db.ends_with("rug.sqlite"));
+    }
 }
